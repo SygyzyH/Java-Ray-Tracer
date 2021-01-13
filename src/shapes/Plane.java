158c36d44c446a7c616c8600ad9d7277bdb0620e
SygyzyH <76969579+SygyzyH@users.noreply.github.com>
2021-01-13 13:12:59 +0200

    Support for texture rendering
    Added support for texturing a plane

--- src/shapes/Plane.java
+++ src/shapes/Plane.java
@@ -67,7 +67,32 @@ public class Plane extends Shape {
 
     @Override
     public Color getTextureColor(Vec3 p) {
-        // TODO
-        return null;
+        double u, v;
+
+        // find a point e1 such that its not perpendicular to the normal
+        Vec3 e1 = this.norm.cross(new Vec3(1, 0, 0)).get_normalized();
+
+        // if the e1 guess is perpendicular to the normal anyway, take a different one.
+        if (e1 == new Vec3(0, 0, 0))
+            // we can just take a line normal that is on a diffrent axis, that way we can be sure it cannot be perpendicular
+            e1 = this.norm.cross(new Vec3(0, 0, 1)).get_normalized();
+
+        // get another vector representing the second remaining axis of the plane
+        Vec3 e2 = this.norm.cross(e1).get_normalized();
+
+        // get the point on these two axes
+        u = e1.dot(p);
+        v = e2.dot(p);
+
+        // modulo to get the image repeated on the plane
+        u = u % this.visualProperty.texture.getWidth();
+        v = v % this.visualProperty.texture.getHeight();
+
+        // negative values have no meaning if the image is repeated towards infinity in all directions
+        u = Math.abs(u);
+        v = Math.abs(v);
+
+        // convert the color value in the image
+        return this.visualProperty.getTextureColorAt(u, v);
     }
 }
