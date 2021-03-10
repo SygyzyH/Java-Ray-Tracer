e6860940b7bb8931c4594b1f1ffd7a0555b76a8a
SygyzyH <76969579+SygyzyH@users.noreply.github.com>
2021-03-10 14:29:00 +0200

    Optimized to use fast inverse square root

--- src/mathematics/Vec3.java
+++ src/mathematics/Vec3.java
@@ -47,19 +47,14 @@ public class Vec3 {
     }
 
     public double length() {
-        return Math.sqrt(this.x * this.x + this.y * y + this.z * this.z);
+        if (this.length == Double.NEGATIVE_INFINITY)
+            this.length = 1 / Functions.fastInverseSqrt(this.x * this.x + this.y * y + this.z * this.z);
+        return this.length;
     }
 
     public Vec3 get_normalized() {
-        double len = this.length();
-        return new Vec3(this.x / len, this.y / len, this.z / len);
-    }
-
-    public void normalize() {
-        double len = this.length();
-        this.x /= len;
-        this.y /= len;
-        this.z /= len;
+        double len = Functions.fastInverseSqrt(this.x * this.x + this.y * y + this.z * this.z);
+        return new Vec3(this.x * len, this.y * len, this.z * len);
     }
 
     public Vec3 clone() {
